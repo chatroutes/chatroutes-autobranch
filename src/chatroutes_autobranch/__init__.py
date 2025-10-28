f1d2d7ff69b85e3481df8735dcd69b05a46ab427
Afzal Farooqui <afzal@mednosis.com>
2025-10-27 22:29:51 -0400

    Fix version in __init__.py to 1.3.0

--- src/chatroutes_autobranch/__init__.py
+++ src/chatroutes_autobranch/__init__.py
@@ -10,7 +10,7 @@ This library provides clean, standalone primitives for:
 - Pluggable, testable, replaceable components
 """
 
-__version__ = "1.2.0"
+__version__ = "1.3.0"
 
 # Core dataclasses
 from chatroutes_autobranch.core.candidate import (
